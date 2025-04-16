3731ba5364d711223607b6324d770cd52c1c0d80
Kartala Varun Karthik <kartala.varunkarthik@hexagon.com>
2025-04-16 20:37:02 +0530

    restructured the files and segregated the functionalities to maintain modularity

---
+++ REACT/react-gis-app/src/data/staticData.js
@@ -0,0 +1,55 @@
+export const ParksData = [
+  {
+    name: "Sanjeevaiah Park",
+    location: [17.385, 78.486],
+    icon: null,
+  },
+  {
+    name: "Lubmini Park",
+    location: [17.41, 78.472],
+    icon: null,
+  },
+  {
+    name: "KBR Park",
+    location: [17.42, 78.419],
+    icon: null,
+  },
+  {
+    name: "Botanical Gardens",
+    location: [17.455, 78.36],
+    icon: null,
+  },
+  {
+    name: "Nehru Zoological Park",
+    location: [17.35, 78.452],
+    icon: null,
+  },
+];
+
+export const HospitalsData = [
+  {
+    name: "KIMS Hospital",
+    location: [17.427, 78.404],
+    icon: null,
+  },
+  {
+    name: "Apollo Hospitals, Jubilee Hills",
+    location: [17.415, 78.413],
+    icon: null,
+  },
+  {
+    name: "Kamineni Hospitals, LB Nagar",
+    location: [17.351, 78.555],
+    icon: null,
+  },
+  {
+    name: "Osmania General Hopsital",
+    location: [17.371, 78.473],
+    icon: null,
+  },
+  {
+    name: "Gandhi Hopsital",
+    location: [17.423, 78.504],
+    icon: null,
+  },
+];
